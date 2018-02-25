9cd47a3e65f677368c3a2a2320b225456387f1a7
mabo207 <mabo168general+github@gmail.com>
2018-02-25 17:09:45 +0900

    当たり判定による位置ずらしの処理順番について、複数図形と重なっている場合は同じ図形に対して連続で処理を行わないようにした。
    連続で処理をするのは、位置ずらしの割合を大きくする事と同義になってしまい、割合を小さくすることで図形に挟まった時の振動を防ぐという実装意図の意味がなくなるため。

--- ActiveWarSimulation/FieldBase/Shape.cpp
+++ ActiveWarSimulation/FieldBase/Shape.cpp
@@ -27,20 +27,25 @@ void Shape::Update(const std::vector<std::shared_ptr<Shape>> &pShapeVec,size_t u
 	if(m_fix==Fix::e_dynamic){
 		//m_fixによって動くかどうかを決める
 		//この関数が呼ばれた際図形が存在するのは仮移動先である。仮移動先において、自分と重なっている図形を探し、位置をずらす。
-		//重なっている図形の外に出るように押し出し処理をする
+		//まず重なっている図形を全てリストアップする
+		std::vector<std::shared_ptr<const Shape>> inShapeList;
 		for(const std::shared_ptr<const Shape> &pShape:pShapeVec){
 			//当たり判定に影響を与える図形以外に対して押し出し処理を行う
 			if(pShape->m_fix!=Fix::e_ignore){
 				//図形をぴったり収める長方形を利用して簡易的な当たり判定を行う
 				if(JudgeInShapeRect(pShape.get())){
-					//updateTimes回だけ当たり判定処理を行う
-					for(size_t i=0;i<updateTimes;i++){
-						//各図形に対して完全に押し出せる距離を求める
-						Vector2D pushVec=CalculatePushVec(pShape.get());
-						//押し出し距離の一定の割合のベクトルだけ押し出す
-						m_position+=pushVec*Shape::pushRate;
-					}
+					//押し出しする図形に追加
+					inShapeList.push_back(pShape);
 				}
+			}
+		}
+		//簡易的当たり判定によって判定できる、重なっている図形の外に出るように押し出し処理をする
+		for(size_t i=0;i<updateTimes;i++){
+			for(const std::shared_ptr<const Shape> &pShape:inShapeList){
+				//各図形に対して完全に押し出せる距離を求める
+				Vector2D pushVec=CalculatePushVec(pShape.get());
+				//押し出し距離の一定の割合のベクトルだけ押し出す
+				m_position+=pushVec*Shape::pushRate;
 			}
 		}
 	}
