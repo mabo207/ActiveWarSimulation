e5c1d734b05769495f919ef70da47a58968a812b
mabo207 <mabo168general+github@gmail.com>
2018-04-09 22:23:41 +0900

    BattleSceneクラスをバトルパート全般の管理クラスに変更した。

--- ActiveWarSimulation/ActiveWarSimulation/BattleScene.h
+++ ActiveWarSimulation/ActiveWarSimulation/BattleScene.h
@@ -3,44 +3,27 @@
 
 #include"BattleSceneData.h"
 #include"GameScene.h"
-#include"Unit.h"
 
-//バトル場面表すクラス
+//バトルのゲームプレイ場面全般の管理を行うクラス
 class BattleScene:public GameScene{
 	//型・列挙体
 public:
-	struct RouteInfo{
-		const Vector2D pos;
-		const float OP;
-		RouteInfo(Vector2D i_pos,float i_OP):pos(i_pos),OP(i_OP){}
-	};
-
+	
 	//定数
 protected:
-	static const float routeFrequency;//OPをどのくらい消費するたびにm_routeに記録するか
-
+	
 public:
 
 	//変数
 protected:
 	//ゲームの進行を管理する変数
-
+	std::shared_ptr<GameScene> m_sceneData;//一番基底となるシーン
 
 	//ゲームに使用する変数
 	std::shared_ptr<BattleSceneData> m_battleSceneData;//ゲーム内全体で使うデータ
-
-	//ユニット移動場面でのみ使用する変数
-	Unit *m_aimedUnit;//現在の攻撃対象ユニット(静的ポインタ)
-	std::vector<RouteInfo> m_route;//操作ユニットが通った経路
-
+	
 	//関数
 protected:
-	Vector2D CalculateInputVec()const;//味方ターン時は入力されてるアナログスティックのベクトルを返す。敵ターン時はAIに従って移動方向のベクトルを返す。
-	bool PositionUpdate(const Vector2D inputVec);//ユニットの位置を更新、m_operateUnitに移動操作がされればtrueを返す。
-	void FinishUnitOperation();//次のユニットへの遷移処理
-	void SetAimedUnit(float angle=0.0f,int turntimes=1);//m_aimedUnitを変更する。turntimesだけ時計回りに回転させる
-	void ProcessAttack();//攻撃処理を行う
-	bool JudgeAttackCommandUsable()const;//攻撃コマンドを使用可能かどうか
 
 public:
 	BattleScene(const char *stagename);
