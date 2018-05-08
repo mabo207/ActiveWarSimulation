d0d662f3a4a9dcee32e7e930d12d42b3665cb3c7
mabo207 <mabo168general+github@gmail.com>
2018-05-08 13:09:18 +0900

    マウス入力による移動に対応するようにした。

--- ActiveWarSimulation/ActiveWarSimulation/PlayerMoveScene.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/PlayerMoveScene.cpp
@@ -8,14 +8,28 @@ PlayerMoveScene::PlayerMoveScene(std::shared_ptr<BattleSceneData> battleSceneDat
 
 Vector2D PlayerMoveScene::CalculateInputVec()const{
 	Vector2D moveVec;
-	moveVec=analogjoypad_get(DX_INPUT_PAD1);
-	//アナログスティックの物理的なズレ等によるmoveVecの微入力を除く
-	const float gap=50.0f;
-	if(std::abs(moveVec.x)<gap){
-		moveVec.x=0.0f;
-	}
-	if(std::abs(moveVec.y)<gap){
-		moveVec.y=0.0f;
+	if(mouse_get(MOUSE_INPUT_LEFT)>0){
+		//マウスを左クリックしているならマウス入力に対応させる
+		moveVec=GetMousePointVector2D()-m_battleSceneData->m_operateUnit->getPos();
+		//アナログスティックの物理的なズレ等によるmoveVecの微入力を除く
+		const float gap=4.0f;
+		if(std::abs(moveVec.x)<gap){
+			moveVec.x=0.0f;
+		}
+		if(std::abs(moveVec.y)<gap){
+			moveVec.y=0.0f;
+		}
+	} else{
+		//通常はアナログスティック入力
+		moveVec=analogjoypad_get(DX_INPUT_PAD1);
+		//アナログスティックの物理的なズレ等によるmoveVecの微入力を除く
+		const float gap=50.0f;
+		if(std::abs(moveVec.x)<gap){
+			moveVec.x=0.0f;
+		}
+		if(std::abs(moveVec.y)<gap){
+			moveVec.y=0.0f;
+		}
 	}
 	return moveVec;
 }
