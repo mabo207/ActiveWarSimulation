08578753c668c8a0f059d1da24ab7ce1c321d050
mabo207 <mabo168general+github@gmail.com>
2019-05-06 00:04:07 +0900

    射手関連のボーナスを作成し、std::make_pair()を使用するようにした。

--- ActiveWarSimulation/ActiveWarSimulation/ScoreObserver.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/ScoreObserver.cpp
@@ -68,11 +68,11 @@ std::string ScoreObserver::GetScoreExplain()const{
 		turnScore.second=std::max(0,20-clearTurn)*500;
 		//クリアターン数のボーナス得点
 		if(clearTurn<5){
-			bonus.push_back(std::pair<std::string,int>("神速進軍",3000));
+			bonus.push_back(std::make_pair("神速進軍",3000));
 		} else if(clearTurn<7){
-			bonus.push_back(std::pair<std::string,int>("速攻進軍",2000));
+			bonus.push_back(std::make_pair("速攻進軍",2000));
 		} else if(clearTurn>15){
-			bonus.push_back(std::pair<std::string,int>("ノロノロ進軍",100));
+			bonus.push_back(std::make_pair("ノロノロ進軍",100));
 		}
 	}
 	//生存数
@@ -98,9 +98,9 @@ std::string ScoreObserver::GetScoreExplain()const{
 		}
 		//生存数のボーナス得点
 		if(totalPlayerUnitCount-livePlayerUnitCount==0){
-			bonus.push_back(std::pair<std::string,int>("全員生存",3000));
+			bonus.push_back(std::make_pair("全員生存",3000));
 		} else if(livePlayerUnitCount==1){
-			bonus.push_back(std::pair<std::string,int>("ほぼ引き分け",200));
+			bonus.push_back(std::make_pair("ほぼ引き分け",200));
 		}
 	}
 	//被ダメージ%推移
@@ -136,13 +136,13 @@ std::string ScoreObserver::GetScoreExplain()const{
 			}
 			//最小HP割合が求まれば、それに応じたボーナスを出す
 			if(minMinRate>=1.00){
-				bonus.push_back(std::pair<std::string,int>("ノーダメージ",5000));
+				bonus.push_back(std::make_pair("ノーダメージ",5000));
 			} else if(minMinRate>=0.75){
-				bonus.push_back(std::pair<std::string,int>("HP75%キープ",2000));
+				bonus.push_back(std::make_pair("HP75%キープ",2000));
 			} else if(minMinRate>=0.50){
-				bonus.push_back(std::pair<std::string,int>("HP50%キープ",1000));
+				bonus.push_back(std::make_pair("HP50%キープ",1000));
 			} else if(maxMinRate<=0.25){
-				bonus.push_back(std::pair<std::string,int>("ギリギリの戦い",500));
+				bonus.push_back(std::make_pair("ギリギリの戦い",500));
 			}
 		}
 	}
@@ -151,7 +151,7 @@ std::string ScoreObserver::GetScoreExplain()const{
 		size_t moveCount=0;//手番がきた回数
 		float totalWalkOP=0.0f;//移動に使った合計OP
 		int attackCount=0;//攻撃回数
-		for(const std::shared_ptr<LogElement> logData:m_logList){
+		for(const std::shared_ptr<const LogElement> logData:m_logList){
 			const LogElement::UnitLogData operateData=logData->GetOperateUnitData();
 			if(operateData.punit!=nullptr
 				&& operateData.punit->GetBattleStatus().team==Unit::Team::e_player
@@ -177,14 +177,14 @@ std::string ScoreObserver::GetScoreExplain()const{
 			if(moveCount>0){
 				double averageWalkOP=totalWalkOP/moveCount;
 				if(averageWalkOP>80.0){
-					bonus.push_back(std::pair<std::string,int>("遊撃のプロフェッショナル",1500));
+					bonus.push_back(std::make_pair("遊撃のプロフェッショナル",1500));
 				} else if(averageWalkOP>60.0){
-					bonus.push_back(std::pair<std::string,int>("遊撃兵士",1000));
+					bonus.push_back(std::make_pair("遊撃兵士",1000));
 				}
 			}
 			//攻撃回数
 			if(attackCount>=finishLog->GetClearTurn()){
-				bonus.push_back(std::pair<std::string,int>("生粋のアタッカー",2000));
+				bonus.push_back(std::make_pair("生粋のアタッカー",2000));
 			}
 		}
 	}
@@ -194,24 +194,24 @@ std::string ScoreObserver::GetScoreExplain()const{
 		size_t magicTotalCount=0;//部隊全体で魔法で攻撃された回数
 		size_t physicAttackedCount=0;//物理で重装兵に攻撃された回数
 		size_t physicTotalCount=0;//部隊全体で物理で攻撃された回数
-		for(const std::shared_ptr<LogElement> logData:m_logList){
+		for(const std::shared_ptr<const LogElement> logData:m_logList){
 			if(logData->GetLogKind()==LogElement::LogKind::e_attack){
-				const std::shared_ptr<AttackLog> attackLog=std::dynamic_pointer_cast<AttackLog>(logData);
+				const std::shared_ptr<const AttackLog> attackLog=std::dynamic_pointer_cast<const AttackLog>(logData);
 				if(attackLog.get()!=nullptr
 					&& attackLog->GetOperateUnitData().punit!=nullptr
-					&& attackLog->GetAimedUnitData().punit!=nullptr
-					&& attackLog->GetAimedUnitData().punit->GetBattleStatus().team==Unit::Team::e_player)
+					&& attackLog->GetAimedUnit()!=nullptr
+					&& attackLog->GetAimedUnit()->GetBattleStatus().team==Unit::Team::e_player)
 				{
 					switch(attackLog->GetOperateUnitData().punit->GetBattleStatus().weapon->GetActionKind()){
 					case(WeaponActionKind::e_physicalAttack):
 						physicTotalCount++;
-						if(attackLog->GetAimedUnitData().punit->GetBaseStatus().profession==Unit::Profession::e_armer){
+						if(attackLog->GetAimedUnit()->GetBaseStatus().profession==Unit::Profession::e_armer){
 							physicAttackedCount++;
 						}
 						break;
 					case(WeaponActionKind::e_magicAttack):
 						magicTotalCount++;
-						if(attackLog->GetAimedUnitData().punit->GetBaseStatus().profession==Unit::Profession::e_armer){
+						if(attackLog->GetAimedUnit()->GetBaseStatus().profession==Unit::Profession::e_armer){
 							magicAttackedCount++;
 						}
 						break;
@@ -226,22 +226,85 @@ std::string ScoreObserver::GetScoreExplain()const{
 			//味方に重装兵がいる場合のみボーナス処理をする
 			//魔法被弾回数
 			if(magicAttackedCount==0){
-				bonus.push_back(std::pair<std::string,int>("重装兵で魔法攻撃を受けない",2000));
+				bonus.push_back(std::make_pair("重装兵で魔法攻撃を受けない",2000));
 			} else if(magicAttackedCount==magicTotalCount){
-				bonus.push_back(std::pair<std::string,int>("重装兵が弱点を突かれ続けた",100));
+				bonus.push_back(std::make_pair("重装兵が弱点を突かれ続けた",100));
 			}
 			//物理被弾回数
 			if(physicTotalCount>0){
 				double rate=1.0*physicAttackedCount/physicTotalCount;
 				if(rate>=0.75){
-					bonus.push_back(std::pair<std::string,int>("物理鉄壁",1500));
+					bonus.push_back(std::make_pair("物理鉄壁",1500));
 				} else if(rate>=0.50){
-					bonus.push_back(std::pair<std::string,int>("物理壁",1000));
+					bonus.push_back(std::make_pair("物理壁",1000));
 				}
 			}
 		}
 	}
 	//射手関連
+	{
+		size_t attackCount=0;//合計攻撃回数
+		size_t effectiveAttackCount=0;//効果的な攻撃の回数（射手・魔道士・衛生兵狙い）
+		float totalAttackLengthRate=0.0f;//攻撃射程割合の総和
+		const auto judgeEffective=[](const Unit *punit){
+			if(punit!=nullptr){
+				switch(punit->GetBaseStatus().profession){
+				case(Unit::Profession::e_archer):
+				case(Unit::Profession::e_mage):
+				case(Unit::Profession::e_healer):
+					return true;
+				}
+			}
+			return false;
+		};
+		for(const std::shared_ptr<LogElement> logData:m_logList){
+			if(logData->GetLogKind()==LogElement::LogKind::e_attack){
+				const std::shared_ptr<const AttackLog> attackLog=std::dynamic_pointer_cast<const AttackLog>(logData);
+				if(attackLog.get()!=nullptr
+					&& attackLog->GetOperateUnitData().punit!=nullptr
+					&& attackLog->GetOperateUnitData().punit->GetBattleStatus().team==Unit::Team::e_player
+					&& attackLog->GetOperateUnitData().punit->GetBaseStatus().profession==Unit::Profession::e_archer
+					&& attackLog->GetAimedUnit()!=nullptr)
+				{
+					attackCount++;
+					//効果的な攻撃であればeffectiveAttackCountを加算
+					if(judgeEffective(attackLog->GetAimedUnit())){
+						effectiveAttackCount++;
+					}
+					//攻撃射程割合を調べる
+					const Vector2D vec=attackLog->GetOperateUnitData().punit->getPos()-attackLog->GetAimedUnit()->getPos();
+					const float maxLength=attackLog->GetOperateUnitData().punit->GetBattleStatus().weapon->GetLength();
+					totalAttackLengthRate+=(vec.size()-Unit::unitCircleSize)/maxLength;
+				}
+			}
+		}
+		const auto judgeExistArcher=[&judgeExistProfession](const LogElement::UnitLogData &logData){
+			return judgeExistProfession(logData,Unit::Profession::e_archer);
+		};
+		if(initLog->JudgeEveryUnitData(judgeExistArcher,false)){
+			//アーチャーが自軍にいればボーナス計算
+			//攻撃射程
+			if(attackCount>0){
+				const double averageRate=totalAttackLengthRate/attackCount;
+				if(averageRate>=0.80){
+					bonus.push_back(std::make_pair("狙撃手",2000));
+				} else if(averageRate>=0.60){
+					bonus.push_back(std::make_pair("後衛",1000));
+				}
+			}
+			//狙う兵種
+			size_t effectiveUnitCount=0;//効果的な攻撃相手の数
+			for(const LogElement::UnitLogData &logData:initLog->m_unitDataList){
+				if(judgeEffective(logData.punit)){
+					effectiveUnitCount++;
+				}
+			}
+			if(effectiveAttackCount>0 && effectiveAttackCount>=effectiveUnitCount){
+				//効果的な攻撃相手に１回ずつ攻撃しているなら（ただし、厳密にはせず回数の合計だけみる）
+				bonus.push_back(std::make_pair("弱点狙いの射手",1500));
+			}
+		}
+	}
 
 	return "";
 }
