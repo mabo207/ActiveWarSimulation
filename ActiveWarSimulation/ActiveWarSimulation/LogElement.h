6166f4ac94a96fb4074394c587451911148f3975
mabo207 <mabo168general+github@gmail.com>
2019-05-05 14:02:30 +0900

    ログ収集の際に、ユニットへのポインタを管理するようにした。
    ポインタが分かれば死んだユニットについて位置を調べることも可能なので、これでゲームの移動の再現が取れるはず。

--- ActiveWarSimulation/ActiveWarSimulation/LogElement.h
+++ ActiveWarSimulation/ActiveWarSimulation/LogElement.h
@@ -19,33 +19,29 @@ public:
 	};
 	//ユニット1体についてのデータ形式
 	struct UnitLogData{
-		UnitLogData(int i_hp,float i_op,const Vector2D &i_pos,Unit::Profession::Kind i_profession,const std::string &i_name)
+		UnitLogData(int i_hp,float i_op,const Vector2D &i_pos,const Unit * const i_punit)
 			:hp(i_hp)
 			,op(i_op)
 			,pos(i_pos)
-			,profession(i_profession)
-			,name(i_name)
+			,punit(i_punit)
 		{}
 		explicit UnitLogData(const Unit * const punit)
 			:UnitLogData(punit->GetBattleStatus().HP
 				,punit->GetBattleStatus().OP
 				,punit->getPos()
-				,punit->GetBaseStatus().profession
-				,punit->GetBaseStatus().name)
+				,punit)
 		{}
 		bool operator==(const UnitLogData &otherObj)const{
 			return (this->hp==otherObj.hp
 				&& this->op==otherObj.op
 				&& this->pos==otherObj.pos
-				&& this->profession==otherObj.profession
-				&& this->name==otherObj.name);
+				&& this->punit==otherObj.punit);
 		}
 		//変数を追加した場合は==やコンストラクタの変更を忘れずに！
 		const int hp;
 		const float op;
 		const Vector2D pos;
-		const Unit::Profession::Kind profession;
-		const std::string name;
+		const Unit * const punit;
 	};
 
 	LogElement(LogKind kind,const std::shared_ptr<const BattleSceneData> battleData);
