397ca6c24b05158521b5d705768f016a96283c50
mabo207 <mabo168general+github@gmail.com>
2019-05-22 18:12:47 +0900

    テストプロジェクトの作成

---
+++ ActiveWarSimulation/UnitTest_StringBuilder/unittest1.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include"../ActiveWarSimulation/StringBuilder.h"
+#include"../ActiveWarSimulation/FileRead.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest_StringBuilder
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: テスト コードをここに挿入します
+			const std::string testStr=FileStrRead("../ActiveWarSimulation/SaveData/scoreRanking.txt");
+			const char topSpliter='\n'
+				,topBeginer='{'
+				,topEnder='}'
+				,inSpliter=','
+				,inBeginer='('
+				,inEnder=')';
+			const StringBuilder oldBuilder(testStr,topSpliter,topBeginer,topEnder,false,true);
+			const NewSB newBuilder(std::shared_ptr<const std::string>(new std::string(testStr)),testStr.size(),topSpliter,topBeginer,topEnder,'\0','\0',0,false,true);
+			Assert::AreEqual(oldBuilder.m_vec.size(),newBuilder.m_vec.size());
+		}
+	};
+}
