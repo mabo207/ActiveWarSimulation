41eb3b73b9c2a78af28a6db3327930ca4b5ad702
mabo207 <mabo168general+github@gmail.com>
2019-05-23 16:53:10 +0900

    FileRead()関数を消した
    必要ない

--- ActiveWarSimulation/ActiveWarSimulation/FileRead.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/FileRead.cpp
@@ -5,62 +5,6 @@
 #include<climits>
 
 
-//ファイルを読み込みstd::vector<char>型にして返す
-std::vector<char> FileRead(const char *DatabaseName){
-	//ファイルを開く
-	std::ifstream ifs(DatabaseName);
-	if(!ifs){
-		assert(false);
-		return std::vector<char>();
-	}
-	//文字を読み込む
-	std::vector<char> OriginalStr;
-	while(true){
-		char word=ifs.get();
-		if(word==EOF){//読み取ったものがEOFになるまでループする
-			break;
-		}
-		OriginalStr.push_back(word);
-	}
-
-	//ファイルを閉じる
-	ifs.close();
-
-	return OriginalStr;
-}
-
-//ファイルを読み込み、改行・カンマで区切られた部分を区別して返す(特化形式)
-std::vector<std::vector<std::string>> CommaCutRead(const char *DatabaseName){
-	//ファイルの読み込み
-	std::vector<char> OriginalData=FileRead(DatabaseName);
-	//改行毎にデータを分けて、１行内についてカンマ区切りで文字列を読み取る
-	std::vector<std::vector<std::string>> CutData;//こいつを返す
-	std::vector<std::string> LineData;//１行分のデータ
-	std::string Str="";//LineDataに入れる文字列
-	for(unsigned int i=0;i<OriginalData.size();i++){
-		switch(OriginalData[i]){
-		case('\n')://改行文字の時
-			if(Str!=""){
-				LineData.push_back(Str);//Strに何か書いてあればLineDataに加える
-			}
-			CutData.push_back(LineData);//CutDataに挿入
-			//初期化
-			LineData.clear();
-			Str.clear();
-			break;
-		case(',')://カンマの時
-			LineData.push_back(Str);//LineDataに挿入
-			//初期化
-			Str.clear();
-			break;
-		default://それ以外の時
-			Str=Str+OriginalData[i];//文字列に文字を追加
-			break;
-		}
-	}
-	return CutData;
-}
-
 //ファイルを読み込みstd::string型にして返す
 std::string FileStrRead(const char *DatabaseName){
 	//ファイルを開く
@@ -95,7 +39,39 @@ std::string FileStrRead(const char *DatabaseName){
 	return OriginalStr;
 }
 
-//CSVフォルダを読み取ってstd::vector<std::vector<int>>型にして返す(特化形式)
+//ファイルを読み込み、改行・カンマで区切られた部分を区別して返す
+std::vector<std::vector<std::string>> CommaCutRead(const char *DatabaseName){
+	//ファイルの読み込み
+	const std::string OriginalData=FileStrRead(DatabaseName);
+	//改行毎にデータを分けて、１行内についてカンマ区切りで文字列を読み取る
+	std::vector<std::vector<std::string>> CutData;//こいつを返す
+	std::vector<std::string> LineData;//１行分のデータ
+	std::string Str="";//LineDataに入れる文字列
+	for(const char c:OriginalData){
+		switch(c){
+		case('\n')://改行文字の時
+			if(Str!=""){
+				LineData.push_back(Str);//Strに何か書いてあればLineDataに加える
+			}
+			CutData.push_back(LineData);//CutDataに挿入
+			//初期化
+			LineData.clear();
+			Str.clear();
+			break;
+		case(',')://カンマの時
+			LineData.push_back(Str);//LineDataに挿入
+			//初期化
+			Str.clear();
+			break;
+		default://それ以外の時
+			Str.push_back(c);//文字列に文字を追加
+			break;
+		}
+	}
+	return CutData;
+}
+
+//CSVフォルダを読み取ってstd::vector<std::vector<int>>型にして返す
 std::vector<std::vector<int>> CSVRead(const char *DatabaseName){
 	//カンマ区切りで読み取る
 	std::vector<std::vector<std::string>> DatabaseStr=CommaCutRead(DatabaseName);
--- ActiveWarSimulation/ActiveWarSimulation/FileRead.h
+++ ActiveWarSimulation/ActiveWarSimulation/FileRead.h
@@ -4,13 +4,15 @@
 //インクルード等
 #include<vector>
 #include<string>
-#include<memory>
 
 //関数
 //ファイルを読み込みstring型にして返す
 std::string FileStrRead(const char *DatabaseName);
 
-//CSVフォルダを読み取ってvector<vector<int>>型にして返す(特化形式)
+//ファイルを読み込み、改行・カンマで区切られた部分を区別して返す
+std::vector<std::vector<std::string>> CommaCutRead(const char *DatabaseName);
+
+//CSVフォルダを読み取ってvector<vector<int>>型にして返す
 std::vector<std::vector<int>> CSVRead(const char *DatabaseName);
 
 #endif
