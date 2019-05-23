101c3571d83e478aa3a066b01705a7b1a60a650f
mabo207 <mabo168general+github@gmail.com>
2019-05-23 16:07:49 +0900

    いらない記述や合っていない記述を削る

--- ActiveWarSimulation/ActiveWarSimulation/FileRead.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/FileRead.cpp
@@ -4,16 +4,16 @@
 #include"FileRead.h"
 
 
-//ファイルを読み込みvector<char>型にして返す
-vector<char> FileRead(const char *DatabaseName){
+//ファイルを読み込みstd::vector<char>型にして返す
+std::vector<char> FileRead(const char *DatabaseName){
 	//ファイルを開く
-	ifstream ifs(DatabaseName);
+	std::ifstream ifs(DatabaseName);
 	if(!ifs){
 		assert(false);
-		return vector<char>();
+		return std::vector<char>();
 	}
 	//文字を読み込む
-	vector<char> OriginalStr;
+	std::vector<char> OriginalStr;
 	while(true){
 		char word=ifs.get();
 		if(word==EOF){//読み取ったものがEOFになるまでループする
@@ -29,13 +29,13 @@ vector<char> FileRead(const char *DatabaseName){
 }
 
 //ファイルを読み込み、改行・カンマで区切られた部分を区別して返す(特化形式)
-vector<vector<string>> CommaCutRead(const char *DatabaseName){
+std::vector<std::vector<std::string>> CommaCutRead(const char *DatabaseName){
 	//ファイルの読み込み
-	vector<char> OriginalData=FileRead(DatabaseName);
+	std::vector<char> OriginalData=FileRead(DatabaseName);
 	//改行毎にデータを分けて、１行内についてカンマ区切りで文字列を読み取る
-	vector<vector<string>> CutData;//こいつを返す
-	vector<string> LineData;//１行分のデータ
-	string Str="";//LineDataに入れる文字列
+	std::vector<std::vector<std::string>> CutData;//こいつを返す
+	std::vector<std::string> LineData;//１行分のデータ
+	std::string Str="";//LineDataに入れる文字列
 	for(unsigned int i=0;i<OriginalData.size();i++){
 		switch(OriginalData[i]){
 		case('\n')://改行文字の時
@@ -60,26 +60,26 @@ vector<vector<string>> CommaCutRead(const char *DatabaseName){
 	return CutData;
 }
 
-//ファイルを読み込みstring型にして返す
-string FileStrRead(const char *DatabaseName){
+//ファイルを読み込みstd::string型にして返す
+std::string FileStrRead(const char *DatabaseName){
 	//ファイルの読み込み
-	vector<char> OriginalData=FileRead(DatabaseName);
-	//string型に直す
-	string Str;
+	std::vector<char> OriginalData=FileRead(DatabaseName);
+	//std::string型に直す
+	std::string Str;
 	for(unsigned int i=0;i<OriginalData.size();i++){
 		Str=Str+OriginalData[i];
 	}
 	return Str;
 }
 
-//CSVフォルダを読み取ってvector<vector<int>>型にして返す(特化形式)
-vector<vector<int>> CSVRead(const char *DatabaseName){
+//CSVフォルダを読み取ってstd::vector<std::vector<int>>型にして返す(特化形式)
+std::vector<std::vector<int>> CSVRead(const char *DatabaseName){
 	//カンマ区切りで読み取る
-	vector<vector<string>> DatabaseStr=CommaCutRead(DatabaseName);
+	std::vector<std::vector<std::string>> DatabaseStr=CommaCutRead(DatabaseName);
 	//int型に直す
-	vector<vector<int>> Database;
+	std::vector<std::vector<int>> Database;
 	for(unsigned int i=0;i<DatabaseStr.size();i++){
-		vector<int> Data;//ここに１行分を入れる
+		std::vector<int> Data;//ここに１行分を入れる
 		for(unsigned int n=0;n<DatabaseStr[i].size();n++){//i行目のデータを作る
 			Data.push_back(atoi(DatabaseStr[i][n].c_str()));
 		}
--- ActiveWarSimulation/ActiveWarSimulation/FileRead.h
+++ ActiveWarSimulation/ActiveWarSimulation/FileRead.h
@@ -4,15 +4,13 @@
 //インクルード等
 #include<vector>
 #include<string>
-#include<map>
-
-using namespace std;
+#include<memory>
 
 //関数
 //ファイルを読み込みstring型にして返す
-string FileStrRead(const char *DatabaseName);
+std::string FileStrRead(const char *DatabaseName);
 
 //CSVフォルダを読み取ってvector<vector<int>>型にして返す(特化形式)
-vector<vector<int>> CSVRead(const char *DatabaseName);
+std::vector<std::vector<int>> CSVRead(const char *DatabaseName);
 
 #endif
