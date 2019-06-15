4ce3440f1bbc2a42c90c6866b4ec8c932644e6fd
mabo207 <mabo168general+github@gmail.com>
2019-06-15 15:26:43 +0900

    マウスでステージ選択できるようにした。
    円の大きさにもよるだろうけど、マウスが円の中に入っているかだけでなく、直前フレームからのマウスの軌跡が円を通過したかも用いて判定したほうが良いかもしれない。
    絵が仕上がってから考慮する。

--- ActiveWarSimulation/ActiveWarSimulation/StageSelectUIInStageSelect.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/StageSelectUIInStageSelect.cpp
@@ -15,25 +15,41 @@ StageSelectUIInStageSelect::~StageSelectUIInStageSelect(){}
 BaseUIInStageSelect::UpdateResult StageSelectUIInStageSelect::Update(){
 	//選択ステージの更新
 	bool indexUpdate=false;
+	bool mouseInStage=false;//マウスがステージを示す円の中に入っているかどうか
 	if(!m_controledData.expired()){
 		const std::shared_ptr<ControledData> lock=m_controledData.lock();
 		const size_t beforeIndex=lock->stageIndex;//効果音再生の可否判定に用いる
 		if(m_stageNum>0){
+			//十字キーでの切り替え
 			if(keyboard_get(KEY_INPUT_LEFT)==1){
 				lock->stageIndex=(lock->stageIndex+m_stageNum-1)%m_stageNum;
 			} else if(keyboard_get(KEY_INPUT_RIGHT)==1){
 				lock->stageIndex=(lock->stageIndex+1)%m_stageNum;
+			} else{
+				//マウスでの切り替え
+				const Vector2D mousePos=GetMousePointVector2D();
+				const float circleSize=30.0f;//当たり判定の円の大きさ
+				for(size_t i=0;i<m_stageNum;i++){
+					if((m_stageInfoVec[i].m_pos-mousePos).sqSize()<=circleSize*circleSize){
+						lock->stageIndex=i;
+						mouseInStage=true;
+						break;
+					}
+				}
 			}
 		}
 		indexUpdate=(lock->stageIndex!=beforeIndex);//indexが更新されているならtrueに
 	}
+	//選択以外の入力処理
 	if(indexUpdate){
 		//変更があれば効果音再生
 		PlaySoundMem(GeneralPurposeResourceManager::selectSound,DX_PLAYTYPE_BACK,TRUE);
 	} else{
 		//以下の遷移は選択ステージの変更が行われていない時のみできる
 		//ステージの決定
-		if(keyboard_get(KEY_INPUT_Z)==1){
+		if(keyboard_get(KEY_INPUT_Z)==1
+			|| (mouse_get(MOUSE_INPUT_LEFT)==1 && mouseInStage))
+		{
 			//決定音を出す
 			PlaySoundMem(GeneralPurposeResourceManager::decideSound,DX_PLAYTYPE_BACK,TRUE);
 			//遷移
