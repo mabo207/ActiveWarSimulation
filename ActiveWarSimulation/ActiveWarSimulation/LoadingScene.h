d1e4475b4b8c47346956434ab783c2436a6bbe40
mabo207 <mabo168general+github@gmail.com>
2019-06-22 17:40:44 +0900

    【記録用コード】std::threadを用いてロード中も画面が更新されている演出を実装してみて、バグった
    Dxlibの多くの関数はスレッドセーフでなく、LoadGraph()とかもスレッドセーフでないみたい。（https://dxlib.xsrv.jp/cgi/patiobbs/patio.cgi?mode=past&no=2498）
    別スレッドでLoadGraph()をしてしまっているので、LoadGraph()で書き込みアクセスのエラーが発生していたり、読み込んだ画像の一部が切れていたりするのが20%くらいの確立で発生していた。
    画像の非同期読み込みはSetUseASyncLoadFlag()を用いて実装しないといけないのだが、自分はファイル読み込みをFileRead_gets()等でなくstd::ifstreamで行ってしまっていてこちらの読み込みはstd::threadを用いないと非同期にできない。
    結構根本の部分から実装を変えないといけなくなる。

---
+++ ActiveWarSimulation/ActiveWarSimulation/LoadingScene.h
@@ -0,0 +1,40 @@
+#ifndef DEF_LOADINGSCENE_H
+#define DEF_LOADINGSCENE_H
+
+#include"GameScene.h"
+#include<thread>
+#include<mutex>
+
+//ロード中に少しの画面変更をしたい時の繋ぎの場面
+//スレッドを用いて実現する
+class LoadingScene:public GameScene{
+public:
+	class LoadingSceneFactory:public SceneFactory{
+	public:
+		LoadingSceneFactory(const std::shared_ptr<GameScene::SceneFactory> &nextFactory);
+		~LoadingSceneFactory();
+		std::shared_ptr<GameScene> CreateScene()const{
+			return std::shared_ptr<GameScene>(new LoadingScene(m_nextFactory));
+		}
+	private:
+		const std::shared_ptr<GameScene::SceneFactory> m_nextFactory;
+	};
+
+	virtual ~LoadingScene();
+	int Calculate();
+	void Draw()const;
+
+protected:
+	LoadingScene(const std::shared_ptr<GameScene::SceneFactory> &nextFactory);
+	std::shared_ptr<GameScene> VGetNextScene(const std::shared_ptr<GameScene> &thisSharedPtr)const;
+	
+private:
+	FpsMeasuring m_timer;	//描画用のタイマー
+	std::thread m_loadingThread;	//次の場面を読み込むスレッド
+	//以下は全てm_loadingThreadで変更されるので、スレッド作成以降はmutexを用いてアクセスする
+	std::shared_ptr<GameScene> m_nextScene;	//読み込み先
+	std::mutex m_loadingThreadMutex;	//m_loadingThreadの初期段階でlockされ、終わるとunlockされる
+	bool m_loadingEnd;	//読み込みが終わったかどうか（mutexの状態だけでは、スレッドが動き始める前にmutexでの状態判定が行われたら意図しない動作になってしまうため）
+};
+
+#endif // !DEF_LOADINGSCENE_H
