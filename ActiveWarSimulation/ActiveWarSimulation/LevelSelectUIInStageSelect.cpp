de68a02d9d1851026651e9ad94069541b8490820
mabo207 <mabo168general+github@gmail.com>
2019-07-08 00:20:18 +0900

    レベル決定または戻るの後に、項目がスライドアウトする演出を追加

--- ActiveWarSimulation/ActiveWarSimulation/LevelSelectUIInStageSelect.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/LevelSelectUIInStageSelect.cpp
@@ -28,6 +28,8 @@ LevelSelectUIInStageSelect::LevelSelectUIInStageSelect(const std::weak_ptr<Contr
 		,MouseButtonUI(m_levelButtonX.GetX(),110+StageInfoInStageSelect::levelBoxHeight,StageInfoInStageSelect::levelBoxWidth,StageInfoInStageSelect::levelBoxHeight,-1)
 		,MouseButtonUI(m_levelButtonX.GetX(),130+StageInfoInStageSelect::levelBoxHeight*2,StageInfoInStageSelect::levelBoxWidth,StageInfoInStageSelect::levelBoxHeight,-1)
 		,MouseButtonUI(m_levelButtonX.GetX(),150+StageInfoInStageSelect::levelBoxHeight*3,StageInfoInStageSelect::levelBoxWidth,StageInfoInStageSelect::levelBoxHeight,-1)}
+	,m_afterTransitionAction(false)
+	,m_transitionResult(UpdateResult::e_gotoStageSelect)//テキトーに初期化
 	,m_stageNameFont(stageNameFont)
 	,m_explainFont(explainFont)
 {}
@@ -44,53 +46,60 @@ BaseUIInStageSelect::UpdateResult LevelSelectUIInStageSelect::Update(){
 			ui.GetButtonInfo(nullptr,&y,nullptr,nullptr);
 			ui.WarpTo(m_levelButtonX.GetX(),y);
 		}
+	} else if(m_afterTransitionAction){
+		//項目決定後に移動が終わったら遷移
+		return m_transitionResult;
 	}
-	//レベル選択更新処理
-	bool levelUpdate=false;
-	bool mouseInLevelBox=false;
-	bool levelBoxTap=false;
-	const Vector2D mousePos=GetMousePointVector2D();//現在のフレームのマウスの位置
-	if(!m_controledData.expired()){
-		const std::shared_ptr<ControledData> lock=m_controledData.lock();
-		const StageLevel beforeLevel=lock->selectLevel;
-		if(keyboard_get(KEY_INPUT_UP)==1){
-			lock->selectLevel=beforeLevel.Shift(-1);
-		} else if(keyboard_get(KEY_INPUT_DOWN)==1){
-			lock->selectLevel=beforeLevel.Shift(1);
-		} else{
-			//マウスによる更新処理
-			for(size_t i=0;i<StageLevel::levelCount;i++){
-				if(m_levelButton[i].JudgeIn()){
-					if((mousePos-m_beforeFrameMousePos).sqSize()>=4.0f){
-						//一定距離以上マウスを動かさないと更新されない
-						lock->selectLevel=StageLevel::levelArray[i];
+	//レベル選択更新処理(m_afterTransitionAction中は更新されない)
+	if(!m_afterTransitionAction){
+		bool levelUpdate=false;
+		bool mouseInLevelBox=false;
+		bool levelBoxTap=false;
+		const Vector2D mousePos=GetMousePointVector2D();//現在のフレームのマウスの位置
+		if(!m_controledData.expired()){
+			const std::shared_ptr<ControledData> lock=m_controledData.lock();
+			const StageLevel beforeLevel=lock->selectLevel;
+			if(keyboard_get(KEY_INPUT_UP)==1){
+				lock->selectLevel=beforeLevel.Shift(-1);
+			} else if(keyboard_get(KEY_INPUT_DOWN)==1){
+				lock->selectLevel=beforeLevel.Shift(1);
+			} else{
+				//マウスによる更新処理
+				for(size_t i=0;i<StageLevel::levelCount;i++){
+					if(m_levelButton[i].JudgeIn()){
+						if((mousePos-m_beforeFrameMousePos).sqSize()>=4.0f){
+							//一定距離以上マウスを動かさないと更新されない
+							lock->selectLevel=StageLevel::levelArray[i];
+						}
+						mouseInLevelBox=(lock->selectLevel==StageLevel::levelArray[i]);//マウスが指しているレベルと選択しているレベルが一致しているか
+						break;
 					}
-					mouseInLevelBox=(lock->selectLevel==StageLevel::levelArray[i]);//マウスが指しているレベルと選択しているレベルが一致しているか
-					break;
 				}
 			}
+			levelUpdate=!(lock->selectLevel==beforeLevel);//StageLevelには!=が存在しない
 		}
-		levelUpdate=!(lock->selectLevel==beforeLevel);//StageLevelには!=が存在しない
-	}
-	if(levelUpdate){
-		//レベル更新が行われた場合は効果音を鳴らす
-		PlaySoundMem(GeneralPurposeResource::selectSound,DX_PLAYTYPE_BACK,TRUE);
-	} else{
-		//その他のUI処理(レベル更新が行われた時はこれらの処理はしないようにする)
-		if(keyboard_get(KEY_INPUT_Z)==1
-			|| (mouseInLevelBox && mouse_get(MOUSE_INPUT_LEFT)==1))
-		{
-			//次の選択へ
-			PlaySoundMem(GeneralPurposeResource::decideSound,DX_PLAYTYPE_BACK,TRUE);
-			return UpdateResult::e_gotoBattle;
-		} else if(keyboard_get(KEY_INPUT_X)==1 || mouse_get(MOUSE_INPUT_RIGHT)==1){
-			//戻る
-			PlaySoundMem(GeneralPurposeResource::cancelSound,DX_PLAYTYPE_BACK,TRUE);
-			return UpdateResult::e_gotoStageSelect;
+		if(levelUpdate){
+			//レベル更新が行われた場合は効果音を鳴らす
+			PlaySoundMem(GeneralPurposeResource::selectSound,DX_PLAYTYPE_BACK,TRUE);
+		} else{
+			//その他のUI処理(レベル更新が行われた時はこれらの処理はしないようにする)
+			if(keyboard_get(KEY_INPUT_Z)==1
+				|| (mouseInLevelBox && mouse_get(MOUSE_INPUT_LEFT)==1))
+			{
+				//効果音を鳴らす
+				PlaySoundMem(GeneralPurposeResource::decideSound,DX_PLAYTYPE_BACK,TRUE);
+				//決定後の状態にする(バトルへ進む)
+				BecomeAfterTransitionAction(UpdateResult::e_gotoBattle);
+			} else if(keyboard_get(KEY_INPUT_X)==1 || mouse_get(MOUSE_INPUT_RIGHT)==1){
+				//効果音を鳴らす
+				PlaySoundMem(GeneralPurposeResource::cancelSound,DX_PLAYTYPE_BACK,TRUE);
+				//決定後の状態にする(ステージセレクトに戻る)
+				BecomeAfterTransitionAction(UpdateResult::e_gotoStageSelect);
+			}
 		}
+		//m_beforeFrameMousePosの更新
+		m_beforeFrameMousePos=mousePos;
 	}
-	//m_beforeFrameMousePosの更新
-	m_beforeFrameMousePos=mousePos;
 
 	return UpdateResult::e_notTransition;
 }
@@ -123,4 +132,12 @@ void LevelSelectUIInStageSelect::Draw()const{
 			m_levelButton[index].DrawButtonRect(GetColor(196,255,64),FALSE,5);
 		}
 	}
+}
+
+void LevelSelectUIInStageSelect::BecomeAfterTransitionAction(UpdateResult result){
+	m_afterTransitionAction=true;
+	m_transitionResult=result;
+	//m_levelButtonXもm_stageInfoCenterPosも途中で位置の変更は行われないため、初期位置に戻るようにすればよい
+	m_levelButtonX.SetTarget(m_levelButtonX.GetstartX(),true);
+	m_stageInfoCenterPos.SetTarget(m_stageInfoCenterPos.GetstartX(),m_stageInfoCenterPos.GetstartY(),true);
 }
--- ActiveWarSimulation/ActiveWarSimulation/LevelSelectUIInStageSelect.h
+++ ActiveWarSimulation/ActiveWarSimulation/LevelSelectUIInStageSelect.h
@@ -19,11 +19,15 @@ public:
 	void Draw()const;
 
 private:
+	void BecomeAfterTransitionAction(UpdateResult result);//項目をスライドアウトさせる設定処理
+
 	const StageInfoInStageSelect m_stageInfo;
 	Vector2D m_beforeFrameMousePos;//1フレーム前のマウスの位置
 	Easing m_levelButtonX;
 	PositionControl m_stageInfoCenterPos;
 	std::array<MouseButtonUI,StageLevel::levelCount> m_levelButton;
+	bool m_afterTransitionAction;//決定(または戻る)の後、少ししてから遷移するために用意
+	UpdateResult m_transitionResult;//どこに遷移するかの情報
 
 	//参照しているフォント（ここで削除はしない）
 	const int m_stageNameFont;
