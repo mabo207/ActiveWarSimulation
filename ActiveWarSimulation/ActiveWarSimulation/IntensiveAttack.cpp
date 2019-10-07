c00f71a8de4532865d43a9ba4e6abc8e9f977805
mabo207 <mabo168general+github@gmail.com>
2019-10-07 18:30:40 +0900

    例外評価を増やした
    しかしそもそもの問題として、excellent評価が撃破時と初回攻撃時に出てしまうから下手でも最高評価をたたき出せてしまうのでは説がある。

--- ActiveWarSimulation/ActiveWarSimulation/IntensiveAttack.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/IntensiveAttack.cpp
@@ -7,11 +7,15 @@
 int IntensiveAttack::RubricEvaluate(const BattleSceneData * const battleData)const{
 	//- 例外処理
 	//	- 攻撃しない(-1)
+	//	- 撃破時は必ず最大評価(4)
+	//	- 残り1体の時は撃破時以外は評価しない（-1）
+	//	- 残り2体の時は最高評価でない時に評価2とする。(2)
 	//- 評価
 	//	1. 攻撃相手よりHP割合の低いユニットが3体以上いる
 	//	2. 攻撃相手よりHP割合の低いユニットが2体いる
 	//	3. 攻撃相手よりHP割合の低いユニットが1体いる
 	//	4. 攻撃相手よりHP割合の低いユニットがいない
+	//懸念点として、「そもそもの問題として、excellent評価が撃破時と初回攻撃時に出てしまうから下手でも最高評価をたたき出せてしまうのでは」説がある。
 	const std::shared_ptr<const AttackLog> attackLog=std::dynamic_pointer_cast<const AttackLog>(battleData->m_scoreObserver->GetLatestLog());
 	int evaluate;
 	const int maxEvaluate=3;
@@ -21,56 +25,62 @@ int IntensiveAttack::RubricEvaluate(const BattleSceneData * const battleData)con
 	} else{
 		//撃破できるかの判定
 		const LogElement::UnitLogData aimedData=attackLog->GetAimedUnitData();
+		//全ての敵のHP割合を出す
+		std::vector<std::pair<const Unit *,int>> hpRateVec;
+		for(const LogElement::UnitLogData &unitData:attackLog->m_unitDataList){
+			if(unitData.punit->GetBattleStatus().team==Unit::Team::e_enemy){
+				//全ての敵について格納
+				hpRateVec.push_back(std::make_pair(unitData.punit,32768*unitData.hp/unitData.punit->GetBaseStatus().maxHP));//intで計算するので切り捨てが怖い、ある程度大きい数値をかけることで割合のソートが可能になる。
+			}
+		}
+		//HP割合の低い順にソートする
+		const auto sortFunc=[](const std::pair<const Unit *,int> left,const std::pair<const Unit *,int> right){
+			if(left.second<right.second){
+				return true;
+			} else if(left.second>right.second){
+				return false;
+			}
+			return left.first<right.first;
+		};
+		std::sort(hpRateVec.begin(),hpRateVec.end(),sortFunc);
+		//敵の数を記録(評価に用いる)
+		const size_t enemyNum=hpRateVec.size();
+		//攻撃相手が何番目だったか測定、同率割合に注意
+		size_t order=0;//順位（自分よりHP割合の小さいキャラが何体いるか）を記録、同率を考慮する
+		for(size_t i=0;i<enemyNum;i++){
+			if(hpRateVec[i].first==aimedData.punit){
+				//攻撃相手が見つかった
+				break;
+			} else{
+				//攻撃相手が見つからなかった場合、順位更新する
+				if(i+1<enemyNum && hpRateVec[i].second!=hpRateVec[i+1].second){
+					//[i]のHP割合が[i+1]のHP割合に等しくないなら、[0]~[i]の要素は攻撃相手より順位が上
+					order=i+1;//順位更新([0]から数えるので+1する)
+				}
+			}
+		}
+		//順番と敵数に基づき評価
 		if(aimedData.hp<=attackLog->GetAttackInfo().damage){
 			//撃破していた場合は、残りHP割合の順序に関わらず最大評価を与える
 			evaluate=maxEvaluate;
+		} else if(enemyNum==1){
+			//残り1体の場合は、撃破時以外評価なし
+			evaluate=-1;
+		} else if(enemyNum==2 && order>0){
+			//残り2体の場合は、最大評価でない方の評価を通常と変える
+			evaluate=1;
+		} else if(order==0){
+			//攻撃相手よりHP割合の高いキャラがいない
+			evaluate=maxEvaluate;
+		} else if(order==1){
+			//攻撃相手よりHP割合の高いキャラが1体いる
+			evaluate=2;
+		} else if(order==2){
+			//2体いる
+			evaluate=1;
 		} else{
-			//全ての敵のHP割合を出す
-			std::vector<std::pair<const Unit *,int>> hpRateVec;
-			for(const LogElement::UnitLogData &unitData:attackLog->m_unitDataList){
-				if(unitData.punit->GetBattleStatus().team==Unit::Team::e_enemy){
-					//全ての敵について格納
-					hpRateVec.push_back(std::make_pair(unitData.punit,32768*unitData.hp/unitData.punit->GetBaseStatus().maxHP));//intで計算するので切り捨てが怖い、ある程度大きい数値をかけることで割合のソートが可能になる。
-				}
-			}
-			//HP割合の低い順にソートする
-			const auto sortFunc=[](const std::pair<const Unit *,int> left,const std::pair<const Unit *,int> right){
-				if(left.second<right.second){
-					return true;
-				} else if(left.second>right.second){
-					return false;
-				}
-				return left.first<right.first;
-			};
-			std::sort(hpRateVec.begin(),hpRateVec.end(),sortFunc);
-			//攻撃相手が何番目だったか測定、同率割合に注意
-			size_t order=0;//順位（自分よりHP割合の小さいキャラが何体いるか）を記録、同率を考慮する
-			for(size_t i=0,vecSize=hpRateVec.size();i<vecSize;i++){
-				if(hpRateVec[i].first==aimedData.punit){
-					//攻撃相手が見つかった
-					break;
-				} else{
-					//攻撃相手が見つからなかった場合、順位更新する
-					if(i+1<vecSize && hpRateVec[i].second!=hpRateVec[i+1].second){
-						//[i]のHP割合が[i+1]のHP割合に等しくないなら、[0]~[i]の要素は攻撃相手より順位が上
-						order=i+1;//順位更新([0]から数えるので+1する)
-					}
-				}
-			}
-			//順番に基づき評価
-			if(order==0){
-				//攻撃相手よりHP割合の高いキャラがいない
-				evaluate=maxEvaluate;
-			} else if(order==1){
-				//攻撃相手よりHP割合の高いキャラが1体いる
-				evaluate=2;
-			} else if(order==2){
-				//2体いる
-				evaluate=1;
-			} else{
-				//3体以上
-				evaluate=0;
-			}
+			//3体以上
+			evaluate=0;
 		}
 	}
 
@@ -141,7 +151,7 @@ std::string IntensiveAttack::GetWholeLookBackActionEmpty()const{
 }
 
 std::string IntensiveAttack::GetSubmissionExplanation()const{
-	return "攻撃は最大の防御！\n同じ敵を集中攻撃して素早く敵戦力を削ってみよう！";
+	return "攻撃は最大の防御！\n同じ敵を集中攻撃し素早く敵戦力を削ろう！";
 }
 
 bool IntensiveAttack::JudgeEvaluateOrder(const BattleSceneData * const battleData)const{
