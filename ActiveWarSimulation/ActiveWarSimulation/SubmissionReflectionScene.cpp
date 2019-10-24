40c4cfac02db10edeacb47cb080789fdf6d64189
mabo207 <mabo168general+github@gmail.com>
2019-10-24 13:36:15 +0900

    振り返りシーンの初期化時に描画のために必要な情報を計算するようにした。

--- ActiveWarSimulation/ActiveWarSimulation/SubmissionReflectionScene.cpp
+++ ActiveWarSimulation/ActiveWarSimulation/SubmissionReflectionScene.cpp
@@ -8,6 +8,7 @@
 #include"GeneralPurposeResource.h"
 #include"GraphicControl.h"
 #include"FilePath.h"
+#include"WaitLog.h"
 #include"AttackLog.h"
 
 namespace {
@@ -17,6 +18,56 @@ namespace {
 	const int minimapWidth=(int)(CommonConstParameter::mapSizeX*minimapRate);
 	const int minimapHeight=(int)(CommonConstParameter::mapSizeY*minimapRate);
 }
+//---------------SubmissionReflectionScene::MinimapDrawInfo-----------------
+SubmissionReflectionScene::MinimapDrawInfo::MinimapDrawInfo(const std::shared_ptr<const LogElement> &log)
+	:pOperateUnit(nullptr),pAttackedUnit(nullptr)
+{
+	if(log){
+		//logの有効性判定
+		//関数の作成
+		const std::shared_ptr<const WaitLog> waitLog=std::dynamic_pointer_cast<const WaitLog>(log);
+		const std::shared_ptr<const AttackLog> attackLog=std::dynamic_pointer_cast<const AttackLog>(log);
+		std::function<const Unit *()> getOperateUnit,getAttackedUnit;
+		if(log->GetLogKind()==LogElement::LogKind::e_wait && waitLog){
+			//待機ログの場合
+			getOperateUnit=[&waitLog](){return waitLog->GetOperateUnitData().punit;};
+		} else if(log->GetLogKind()==LogElement::LogKind::e_attack && attackLog){
+			//攻撃ログの場合
+			getOperateUnit=[&attackLog](){return attackLog->GetOperateUnitData().punit;};
+			getAttackedUnit=[&attackLog](){return attackLog->GetAimedUnit();};
+		}
+		//盤面のユニット一覧の作成
+		const size_t maxSize=log->m_unitDataList.size();
+		size_t operateIndex=maxSize,attackedIndex=maxSize,index=0;//pOperateUnitとpAttackedUnitの探索に用いる
+		for(const LogElement::UnitLogData &logData:log->m_unitDataList){
+			Unit u=*logData.punit;
+			u.Warp(logData.pos);
+			u.AddHP(logData.hp-u.GetBattleStatus().HP);
+			u.SetOP(logData.op);
+			u.SetPenetratable(Unit::Team::e_player);
+			//格納
+			unitList.push_back(u);
+			//operateIndex,attackedIndexの探索、関数が存在するかどうか気をつける
+			if(getOperateUnit && logData.punit==getOperateUnit()){
+				operateIndex=index;
+			}
+			if(getAttackedUnit && logData.punit==getAttackedUnit()){
+				attackedIndex=index;
+			}
+			//探索用indexの更新
+			index++;
+		}
+		//pOperateUnitとpAttackedUnitの探索(これ以降unitListの操作は行われない)
+		if(operateIndex<maxSize){
+			pOperateUnit=&unitList[operateIndex];
+		}
+		if(attackedIndex<maxSize){
+			pAttackedUnit=&unitList[attackedIndex];
+		}
+	}
+}
+
+SubmissionReflectionScene::MinimapDrawInfo::~MinimapDrawInfo(){}
 
 //---------------SubmissionReflectionScene-----------------
 SubmissionReflectionScene::SubmissionReflectionScene(const std::shared_ptr<BattleSceneData> &battleSceneData,const std::shared_ptr<BattleSceneElement> &clearScene)
@@ -24,13 +75,18 @@ SubmissionReflectionScene::SubmissionReflectionScene(const std::shared_ptr<Battl
 	,m_battleSceneData(battleSceneData)
 	,m_clearScene(clearScene)
 	,m_operateCursor(LoadGraphEX(FilePath::graphicDir+"operatedCursor.png"))
-{}
+{
+	//m_goodLogInfo,m_badLogInfoの初期化
+	const WholeReflectionInfo reflectionInfo=m_battleSceneData->m_scoreObserver->GetSubmission().GetReflectionInfo();
+	m_goodLogInfo.emplace(reflectionInfo.m_goodLog.second);
+	m_badLogInfo.emplace(reflectionInfo.m_badLog.second);
+}
 
 SubmissionReflectionScene::~SubmissionReflectionScene(){
 	DeleteGraphEX(m_operateCursor);
 }
 
-void SubmissionReflectionScene::DrawResizedMap(int x,int y,const std::vector<Unit> &unitList,const std::shared_ptr<const LogElement> &log)const{
+void SubmissionReflectionScene::DrawResizedMap(int x,int y,const MinimapDrawInfo &minimapInfo)const{
 	//マージンの描画
 	DrawBox(x-lineWidth,y-lineWidth,x+minimapWidth+lineWidth,y+minimapHeight+lineWidth,merginColor,TRUE);
 	const Vector2D startPos((float)x,(float)y);
@@ -38,12 +94,12 @@ void SubmissionReflectionScene::DrawResizedMap(int x,int y,const std::vector<Uni
 	//背景描画
 	DrawExtendGraphExRateAssign(startPos.x,startPos.y,minimapRate,m_battleSceneData->m_mapPic,TRUE);
 	//ユニットの描画
-	for(size_t i=1,siz=unitList.size();i<siz+1;i++){
+	for(size_t i=1,siz=minimapInfo.GetUnitList().size();i<siz+1;i++){
 		const size_t index=i%siz;
 		//ウインドウに入っていない物は描画しない
 		//当たり判定図形のm_fix判定は行わない、そもそも死んでいるかどうかの判定なので、「実際のマップでは死んでいるが仮想マップでは生きているようにしたい」という要望には対応できないから
 		//先頭ユニットは最後に描画する
-		if(m_battleSceneData->m_mapRange->JudgeInShapeRect(&unitList[index])){
+		if(m_battleSceneData->m_mapRange->JudgeInShapeRect(&minimapInfo.GetUnitList()[index])){
 			//アクション範囲の描画フラグ
 			bool actionRangeDraw,animationFlag;
 			if(index==0){
@@ -55,33 +111,29 @@ void SubmissionReflectionScene::DrawResizedMap(int x,int y,const std::vector<Uni
 				animationFlag=false;
 			}
 			//退却したユニット(m_fixがe_ignore)は描画しない
-			unitList[index].DrawUnit(unitList[index].getPos(),startPos,minimapRate,0,animationFlag,true,actionRangeDraw);
+			minimapInfo.GetUnitList()[index].DrawUnit(minimapInfo.GetUnitList()[index].getPos(),startPos,minimapRate,0,animationFlag,true,actionRangeDraw);
 		}
 	}
 	//ユニットのHPゲージの描画
-	for(size_t i=1,siz=unitList.size();i<siz+1;i++){
+	for(size_t i=1,siz=minimapInfo.GetUnitList().size();i<siz+1;i++){
 		const size_t index=i%siz;
-		if(m_battleSceneData->m_mapRange->JudgeInShapeRect(&unitList[index])){
+		if(m_battleSceneData->m_mapRange->JudgeInShapeRect(&minimapInfo.GetUnitList()[index])){
 			//ウインドウに入っていない物は描画しない
-			unitList[index].DrawHPGage(unitList[index].getPos(),startPos,minimapRate);
+			minimapInfo.GetUnitList()[index].DrawHPGage(minimapInfo.GetUnitList()[index].getPos(),startPos,minimapRate);
 		}
 	}
 	//操作ユニットのマーカーを表示
-	if(!unitList.empty()){
+	if(minimapInfo.pOperateUnit!=nullptr){
 		float dx,dy;
 		GetGraphSizeF(m_operateCursor,&dx,&dy);
-		const Vector2D pos=(unitList.begin()->getPos()+Vector2D(-dx/2.0f,-Unit::unitCircleSize-dy+10.0f))*minimapRate+startPos;
+		const Vector2D pos=(minimapInfo.pOperateUnit->getPos()+Vector2D(-dx/2.0f,-Unit::unitCircleSize-dy+10.0f))*minimapRate+startPos;
 		DrawExtendGraphExRateAssign(pos.x,pos.y,minimapRate,m_operateCursor,TRUE);
 	}
 	//ダメージの表示
-	if(log->GetLogKind()==LogElement::LogKind::e_attack){
-		const std::shared_ptr<const AttackLog> attackLog=std::dynamic_pointer_cast<const AttackLog>(log);
-		if(attackLog){
-			//攻撃時は、攻撃相手の上にダメージを表示
-			const LogElement::UnitLogData attackedUnitData=attackLog->GetAimedUnitData();
-			const Vector2D pos=attackedUnitData.pos*minimapRate+startPos;
-			
-		}
+	if(minimapInfo.pOperateUnit!=nullptr && minimapInfo.pAttackedUnit!=nullptr){
+		//攻撃時は、攻撃相手の上にダメージを表示
+		const Vector2D pos=minimapInfo.pAttackedUnit->getPos()*minimapRate+startPos;
+		minimapInfo.pOperateUnit->GetBattleStatus().weapon->DrawPredict((int)pos.x,(int)pos.y,-1,-1,minimapInfo.pOperateUnit,minimapInfo.pAttackedUnit);
 	}
 }
 
@@ -97,31 +149,11 @@ void SubmissionReflectionScene::thisDraw()const{
 	//文章の描画
 	DrawStringToHandle(30,30,"どちらの方が良いとされる行動でしょうか？",GetColor(255,255,255),GeneralPurposeResource::popLargeFont);
 	//比較マップの描画
-	const WholeReflectionInfo reflectionInfo=m_battleSceneData->m_scoreObserver->GetSubmission().GetReflectionInfo();
-	if(reflectionInfo.m_badLog.second && reflectionInfo.m_goodLog.second){
-		std::vector<Unit> goodUnitList,badUnitList;
-		for(const LogElement::UnitLogData &logData:reflectionInfo.m_goodLog.second->m_unitDataList){
-			//良いとされる盤面のユニットの作成
-			Unit u=*logData.punit;
-			u.Warp(logData.pos);
-			u.AddHP(logData.hp-u.GetBattleStatus().HP);
-			u.SetOP(logData.op);
-			u.SetPenetratable(Unit::Team::e_player);
-			//格納
-			goodUnitList.push_back(u);
-		}
-		for(const LogElement::UnitLogData &logData:reflectionInfo.m_badLog.second->m_unitDataList){
-			//悪いとされる盤面のユニットの作成
-			Unit u=*logData.punit;
-			u.Warp(logData.pos);
-			u.AddHP(logData.hp-u.GetBattleStatus().HP);
-			u.SetOP(logData.op);
-			u.SetPenetratable(Unit::Team::e_player);
-			//格納
-			badUnitList.push_back(u);
-		}
-		DrawResizedMap(20,300,goodUnitList,reflectionInfo.m_goodLog.second);
-		DrawResizedMap(980,300,badUnitList,reflectionInfo.m_badLog.second);
+	if(m_goodLogInfo.has_value()){
+		DrawResizedMap(20,300,m_goodLogInfo.value());
+	}
+	if(m_badLogInfo.has_value()){
+		DrawResizedMap(980,300,m_badLogInfo.value());
 	}
 }
 
--- ActiveWarSimulation/ActiveWarSimulation/SubmissionReflectionScene.h
+++ ActiveWarSimulation/ActiveWarSimulation/SubmissionReflectionScene.h
@@ -3,6 +3,7 @@
 
 #include"BattleSceneElement.h"
 #include"BattleSceneData.h"
+#include<optional>
 
 //サブミッション設定時、リザルト画面の前に振り返りを行う場面
 class SubmissionReflectionScene:public BattleSceneElement{
@@ -11,8 +12,22 @@ public:
 	~SubmissionReflectionScene();
 
 private:
+	//型
+	struct MinimapDrawInfo{
+		//ミニマップ描画に必要な情報群
+		explicit MinimapDrawInfo(const std::shared_ptr<const LogElement> &log);
+		~MinimapDrawInfo();
+		const std::vector<Unit>& GetUnitList()const{ return unitList; };//unitListのgetter
+		//unitListの中身のどれかを指すポインタ
+		const Unit *pOperateUnit;
+		const Unit *pAttackedUnit;//nullptrならアクション情報の描画を行わない
+		//描画するユニットの一覧
+	private:
+		std::vector<Unit> unitList;//pOperateUnitとpAttackedUnitがこの要素を指している都合上、外部からの変更を防ぎたいのでprivate
+	};
+
 	//関数
-	void DrawResizedMap(int x,int y,const std::vector<Unit> &unitList,const std::shared_ptr<const LogElement> &log)const;//簡易拡大縮小マップを描画する
+	void DrawResizedMap(int x,int y,const MinimapDrawInfo &minimapInfo)const;//簡易拡大縮小マップを描画する
 	//仮想関数のオーバーライド
 	int thisCalculate();//0:直前場面に戻る 1~END-1:その場面に進む END:バトルパート終了
 	void thisDraw()const;
@@ -22,6 +37,7 @@ private:
 	//変数
 	const std::shared_ptr<BattleSceneData> m_battleSceneData;
 	std::shared_ptr<BattleSceneElement> m_clearScene;//前クラスで作成したClearSceneを一時的に保存するための変数
+	std::optional<MinimapDrawInfo> m_goodLogInfo,m_badLogInfo;//初期化をコンストラクタの{}内で行いたいので遅延の必要がある
 
 	const int m_operateCursor;//操作ユニットを指し示すためのマーカー
 };
