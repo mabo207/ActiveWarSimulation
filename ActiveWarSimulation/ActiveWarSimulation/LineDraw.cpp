84a101398c8b31b09e8feedc09ea46baa6a53967
mabo207 <mabo168general+github@gmail.com>
2019-10-29 15:44:41 +0900

    線を引くワークのたたき台の作成

---
+++ ActiveWarSimulation/ActiveWarSimulation/LineDraw.cpp
@@ -0,0 +1,96 @@
+#include"LineDraw.h"
+#include"DxLib.h"
+#include"input.h"
+#include"ToolsLib.h"
+
+namespace {
+	const float acceptableDistance=5.0f;
+	const float sqAcceptableDistance=acceptableDistance*acceptableDistance;
+}
+
+//---------------ReflectionWork::LineDraw--------------
+ReflectionWork::LineDraw::LineDraw(const std::vector<Edge> &edgeList)
+	:Base(Base::Kind::e_lineDraw)
+	,m_inputing(false)
+	,m_inputIndex(0)//テキトーで良い
+	,m_inputStart(true)//テキトーで良い
+{
+	//m_lineListの初期化
+	const size_t listSize=edgeList.size();
+	m_lineList.reserve(listSize);
+	for(size_t i=0;i<listSize;i++){
+		m_lineList.push_back(edgeList[i]);
+	}
+}
+
+ReflectionWork::LineDraw::~LineDraw(){}
+
+void ReflectionWork::LineDraw::Update(){
+	const Vector2D mouse=GetMousePointVector2D();
+	const int left=mouse_get(MOUSE_INPUT_LEFT);
+	if(m_inputing){
+		//何らかの線を入力中
+		//マウスを離したかの判定
+		if(left==0){
+			//目標点の計算
+			Vector2D target;
+			if(m_inputStart){
+				//start始点であれば目標点はend
+				target=m_lineList[m_inputIndex].edge.GetEndPoint();
+			} else{
+				//end始点であれば目標点はstart
+				target=m_lineList[m_inputIndex].edge.GetBeginPoint();
+			}
+			//ちゃんと引けたかの判定とm_lineListの更新
+			if((target-mouse).sqSize()<=sqAcceptableDistance){
+				m_lineList[m_inputIndex].drawInfo=true;
+			}
+			//m_inputingの更新
+			m_inputing=false;
+		} else{
+			//まだ入力中(何もしない)
+		}
+	} else{
+		//線を探している
+		if(left==1){
+			//クリックした
+			m_inputIndex=0;//初期化
+			for(const size_t listSize=m_lineList.size();m_inputIndex<listSize;m_inputIndex++){
+				//該当する線を探す
+				if((m_lineList[m_inputIndex].edge.GetBeginPoint()-mouse).sqSize()<=sqAcceptableDistance){
+					//始点を入力した
+					m_inputing=true;
+					m_inputStart=true;
+					break;
+				} else if((m_lineList[m_inputIndex].edge.GetEndPoint()-mouse).sqSize()<=sqAcceptableDistance){
+					//終点を入力した
+					m_inputing=true;
+					m_inputStart=false;
+					break;
+				}
+			}
+		}
+	}
+}
+
+bool ReflectionWork::LineDraw::WorkClear()const{
+	//m_lineList[i].drawInfoが全てtrueである時だけtrueを返す
+	for(const LineDrawInfo &line:m_lineList){
+		if(!line.drawInfo){
+			return false;
+		}
+	}
+	return true;
+}
+
+void ReflectionWork::LineDraw::WorkDraw()const{
+	for(const LineDrawInfo &info:m_lineList){
+		if(info.drawInfo){
+			//既に入力済みの直線は実線で
+			info.edge.Shape::Draw(Vector2D(),GetColor(255,128,128),TRUE,5.0f);
+		} else{
+			//まだ入力していない直線は破線で
+
+		}
+	}
+}
--- ActiveWarSimulation/ActiveWarSimulation/LineDraw.h
+++ ActiveWarSimulation/ActiveWarSimulation/LineDraw.h
@@ -2,10 +2,34 @@
 #define DEF_LINEDRAW_H
 
 #include"ReflectionWorkBase.h"
+#include"Edge.h"
+#include<vector>
 
 namespace ReflectionWork{
+	//指定された線を引くワーク
 	class LineDraw:public Base{
+	public:
+		LineDraw(const std::vector<Edge> &edgeList);
+		~LineDraw();
 
+		//仮想関数のオーバーライド
+		void Update();
+		bool WorkClear()const;
+		void WorkDraw()const;
+
+	private:
+		struct LineDrawInfo{
+			//直線とその直線の入力が終わったかの情報をまとめるクラス
+			Edge edge;
+			bool drawInfo;//true:入力した false:まだ入力していない
+			LineDrawInfo(const Edge &i_edge):LineDrawInfo(i_edge,false){}
+			LineDrawInfo(const Edge &i_edge,const bool i_drawInfo):edge(i_edge),drawInfo(i_drawInfo){}
+		};
+
+		std::vector<LineDrawInfo> m_lineList;//指定した線一覧と入力終了情報
+		bool m_inputing;//入力中かどうか
+		size_t m_inputIndex;//どの線を入力しているか
+		bool m_inputStart;//true:startを始点に線を引いている false:end始点
 	};
 }
 
