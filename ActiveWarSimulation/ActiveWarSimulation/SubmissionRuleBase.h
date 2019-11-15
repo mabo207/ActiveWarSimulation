fe8aa53beffba3849008226e3f5919ef13436c27
mabo207 <mabo168general+github@gmail.com>
2019-11-15 17:24:21 +0900

    評価の事前計算用のクラスを作成した
    事前計算を実装したわけではない

--- ActiveWarSimulation/ActiveWarSimulation/SubmissionRuleBase.h
+++ ActiveWarSimulation/ActiveWarSimulation/SubmissionRuleBase.h
@@ -14,9 +14,18 @@ class Unit;
 //サブミッション評価時のルールを表現するための基底クラス
 class SubmissionRuleBase{
 public:
+	struct InAdvanceCalculateData{
+		//事前計算した結果を格納するクラス、継承して用いる
+	};
+
 	virtual ~SubmissionRuleBase()=default;
 	int RubricEvaluate(const BattleSceneData * const battleData)const;//１つの行動についてルーブリック評価をする
 	virtual int RubricEvaluate(const std::vector<BattleObject *> &field,const Vector2D stageSize,const std::shared_ptr<const LogElement> &evaluateLog)const=0;
+	virtual int InAdvanceDataEvaluate(const std::shared_ptr<InAdvanceCalculateData> &,const std::vector<BattleObject *> &field,const Vector2D stageSize,const std::shared_ptr<const LogElement> &evaluateLog)const{
+		//事前計算データを用いて高速評価する関数
+		//デフォルトでは、通常評価を行うようになっている
+		return RubricEvaluate(field,stageSize,evaluateLog);
+	}
 	virtual std::pair<std::string,unsigned int> GetRubricStringInfo(int rubric)const=0;//ルーブリック評価に対する文字列を描画するための情報を返す（文字列＆文字の縁の色）
 	virtual std::string GetWholeLookBack(int mostFrequentEvaluate)const=0;//最も多かったルーブリック評価について、総括的になコメントを返す
 	virtual std::string GetWholeLookBackActionEmpty()const=0;//ルールに該当した行動が何もなかった時の総括的評価の文字列を返す
